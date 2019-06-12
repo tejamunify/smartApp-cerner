d19a4055aeed6f5e5163a71c7ec82aaa26fc3413
krishna teja <tejam@unifytech.com>
2019-06-12 11:15:56 +0530

    created new repository for cerner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'smartApp';
+export class AppComponent implements OnInit {
+  public name: string;
+  public dob: string;
+  public gender: string;
+  public medicationdetails = [];
+  constructor() {}
+
+  ngOnInit() {
+    // window['FHIR'].oauth2.authorize({
+    //   client_id: 'my_web_app',
+    //   scope: 'patient/*.read',
+    // });
+
+    window['FHIR'].oauth2.ready(smart => {
+      smart.patient.read().then(patient => {
+        this.name =
+          patient.name[0].given.join(' ') + ' ' + patient.name[0].family.join('') || '';
+        this.dob = patient.birthDate || '';
+        this.gender = patient.gender || '';
+      });
+      smart.patient.api.search({ type: 'MedicationOrder' }).then(results => {
+        results.data.entry.forEach(element => {
+          this.medicationdetails.push({
+            dosageInstruction: element.resource.dosageInstruction[0].text,
+            name: element.resource.medicationCodeableConcept.text,
+          });
+        });
+      });
+    });
+  }
+
+  getDateOfBirth(date: string) {
+    return new Date(date)
+      .toDateString()
+      .split(' ')
+      .filter((element, index) => index !== 0)
+      .join(' ');
+  }
+
+  getAge(date: string) {
+    const today = new Date();
+    const birthDate = new Date(date);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MatCardModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
